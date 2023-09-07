427b55404c306c16e7ba024f416f96081531ffd0
anastasijamirovic <anastasija.mirovic@elfak.rs>
2023-09-07 23:15:06 +0200

    Update

--- src/mixer_preview.ts
+++ src/mixer_preview.ts
@@ -1,12 +1,6 @@
-//import { Mixer } from './mixer';
-
 export class MixerPreview{
-   // private mixer: Mixer;
-
     constructor(){
-       
         this.draw();
-        
     }
 
     draw(){
@@ -27,27 +21,6 @@ export class MixerPreview{
         speaker_info_frame.innerHTML = `
             <p>Speaker: <span id="speaker-info"></span></p>
         `;
-
-        // const pianoImage = document.getElementById('piano');
-        // const guitarImage = document.getElementById('guitar');
-        // const mixerImage = document.getElementById('mixer');
-    
-        // pianoImage.addEventListener('click', () => {
-        //   // Simulacija klika na klavir
-        //   this.mixer.sendSound('Piano sound data');
-        //   this.updateSpeakerInfo('Piano');
-        // });
-    
-        // guitarImage.addEventListener('click', () => {
-        //   // Simulacija klika na gitaru
-        //   this.mixer.sendSound('Guitar sound data');
-        //   this.updateSpeakerInfo('Guitar');
-        // });
     }
 
-        // private updateSpeakerInfo(instrumentName: string) {
-        //     const speakerInfo = document.getElementById('speaker-info');
-        //     speakerInfo.textContent = instrumentName;
-        //   }
-    
 }
